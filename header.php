b3178d82ca1e247a47981101286efa0f3a4db731
alan-grillo <alan.grillo@orange.fr>
2022-10-15 10:59:07 +0200

    Ajouts php/css
    "Amélioration" du css et essais de header et head avec php

---
+++ header.php
@@ -0,0 +1,9 @@
+<!-- tout le "header de la page du site -->
+<header>
+    <h1 class = "title"> Conception pour le vert</h1>
+    <ul id ="menu-header">
+        <li> Onglet 1</li>
+        <li> Onglet 2 </li>
+        <li> Onglet 3 </li>
+    </ul>
+</header>
---
+++ test.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include "head.php"; ?>
+</head>
+<body>
+<?php include "header.php"; ?>
+<div class="colonne-gauche">
+    <ul class="menu">
+      <li class="menu-element">menu 1</li>
+      <li class="menu-element">menu 2</li>
+    </ul>
+  </div>
+<div class ="liste-des-pratiques">
+    <ul class="ul-des-pratiques">
+      <li id="pratique-une" class="pratique">
+        <h3>Pratique n°1</h3>
+        <p>Il faut baisser le chauffage quand on est pas la</p>
+      </li>
+      <li id="pratique-deux" class ="pratique">
+        <h3>Pratique n°2</h3>
+        <p>Couper l'eau quand on se brosse les dents</p>
+      </li>
+    </ul>
+     
+  </div>
+
+
+</body>
+</html>

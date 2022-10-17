dd579674a6d16c660fa80292e4e9e599e5cf7d7c
Jupiiii <hillereau.clement@orange.fr>
2022-10-17 12:12:52 +0200

    nouveau

--- Index.php
+++ Index.php
@@ -1,16 +1,27 @@
 <!doctype html>
 <html lang="fr">
 <head>
-  <meta charset="utf-8">
-  <title>test</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Sélectionnez vos critères écoresponsable et créer votre projet !">
+  <meta name="keywords" content="eco-conception, bonnes pratiques">
+  <meta name="author" content="Le Cashprize D4G2021">
+  <title>D4G2021 - Le Cashprize team</title>
+  <link rel="shortcut icon" type="image/x-icon" href="images/planete2.png">
   <link rel="stylesheet" href="style.css">
-  <script src="script.js"></script>
 </head>
 <body>
+  <!--Header PHP-->
   <?php require 'header.php';  ?>
 
-  ...
-  <!-- Le reste du contenu -->
-  ...
+  <!-- test text-->
+  <article class="texte">
+    <h1>La team D4G championne</h1>
+    <p>BALABLABLA
+      On va win ça easy
+    </p>
+  </article>
+
 </body>
+
 </html>

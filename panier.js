5e7475b498041843d95f29262152b68e3db64b49
Colin Quétineau <cquetineau.ing2024@esaip.org>
2022-10-27 09:51:14 +0200

    test panier

--- panier.js
+++ panier.js
@@ -1,6 +1,14 @@
-function savebasket(basket){
-    localStorage.saveitem("Panier",panier);
+function creerCookie(nomArticle){
+    //nom de l'article qu'on met dans le panier
+    panier.cookie = nomArticle;
 }
-function getbasket(basket){
-    localStorage.saveitem("Panier");
+function lireCookie(nomArticle){
+    //Récupération nom de l'article sélectionné 
+    var nomArticle = nom;
+    //Création tableau avec tous les articles
+    var listeArticles = panier.cookie.split(';');   
+}
+function afficherPanier(){
+    var afficherArticle = pannier.getElementbyId('cook');
+    affiche.innerHTML = 'L article est :'   + lireCookie('Cookie1'); 
 }

f820bbec0e3c46ff1707f9d95007890cc9a92254
Jupiiii <hillereau.clement@orange.fr>
2022-10-31 14:21:07 +0100

    début "projet"

--- projet.php
+++ projet.php
@@ -4,18 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Sélectionnez vos critères écoresponsable et créer votre projet !">
+    <meta name="keywords" content="eco-conception, bonnes pratiques">
     <meta name="author" content="Le Cashprize D4G2021">
-    <title>D4G2021 - Le Cashprize team</title>
+    <title>D4G2021 - Projet - Le Cashprize team</title>
     <link rel="shortcut icon" type="image/x-icon" href="images/planete2.png">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="print.css" media="print">
 </head>
 
 <body>
     <!--Header PHP-->
-    <?php require 'header.php';  ?>
+    <?php include 'header.php';  ?>
+    <section class="container">
+        <h2 class="titre">Projet</h2>
+    </section>
 
-    
 
+
+    <!--Footer PHP-->
+    <?php include 'footer.php';  ?>
 </body>
 
 </html>

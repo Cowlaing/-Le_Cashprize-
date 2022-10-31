3334a01d554ae98b026d64b7c2207280f4c94a42
Jupiiii <hillereau.clement@orange.fr>
2022-10-31 14:20:51 +0100

    ajout filtres

--- Index.php
+++ Index.php
@@ -21,41 +21,135 @@
   <section class="container">
     <div class="columns">
       <form class="aside">
-        <h2>Sélection filtre</h2>
-        <ul id="liste_filtres">
-          <li>
-            <input type="checkbox" name="type[]" id="spec" value="spec" onclick="gestionFiltre('SPECIFICATIONS')">
-            <label for="spec">Specifications</label>
-          </li>
-          <li>
-            <input type="checkbox" name="type[]" id="strat" value="strat" onclick="gestionFiltre('STRATEGIE')">
-            <label for="strat">Strategie</label>
-          </li>
-          <li>
-            <input type="checkbox" name="type[]" id="ux" value="ux" onclick="gestionFiltre('UX/UI')">
-            <label for="ux">UX/UI</label>
-          </li>
-          <li>
-            <input type="checkbox" name="type[]" id="cont" value="cont" onclick="gestionFiltre('CONTENUS')">
-            <label for="cont">Contenus</label>
-          </li>
-          <li>
-            <input type="checkbox" name="type[]" id="archi" value="archi" onclick="gestionFiltre('ARCHITECTURE')">
-            <label for="archi">Architecture</label>
-          </li>
-          <li>
-            <input type="checkbox" name="type[]" id="front" value="front" onclick="gestionFiltre('FRONTEND')">
-            <label for="front">Frontend</label>
-          </li>
-          <li>
-            <input type="checkbox" name="type[]" id="back" value="back" onclick="gestionFiltre('BACKEND')">
-            <label for="back">Backend</label>
-          </li>
-          <li>
-            <input type="checkbox" name="type[]" id="heb" value="heb" onclick="gestionFiltre('HEBERGEMENT')">
-            <label for="heb">Hébergement</label>
-          </li>
-        </ul>
+        <h2 class="titre">Recherche</h2>
+
+        <fieldset>
+          <legend>Famille</legend>
+          <ul id="liste_filtres">
+            <li>
+              <input type="checkbox" name="type[]" id="spec" value="spec" onclick="gestionFiltre('SPECIFICATIONS')">
+              <label for="spec">Specifications</label>
+            </li>
+            <li>
+              <input type="checkbox" name="type[]" id="strat" value="strat" onclick="gestionFiltre('STRATEGIE')">
+              <label for="strat">Strategie</label>
+            </li>
+            <li>
+              <input type="checkbox" name="type[]" id="ux" value="ux" onclick="gestionFiltre('UX/UI')">
+              <label for="ux">UX/UI</label>
+            </li>
+            <li>
+              <input type="checkbox" name="type[]" id="cont" value="cont" onclick="gestionFiltre('CONTENUS')">
+              <label for="cont">Contenus</label>
+            </li>
+            <li>
+              <input type="checkbox" name="type[]" id="archi" value="archi" onclick="gestionFiltre('ARCHITECTURE')">
+              <label for="archi">Architecture</label>
+            </li>
+            <li>
+              <input type="checkbox" name="type[]" id="front" value="front" onclick="gestionFiltre('FRONTEND')">
+              <label for="front">Frontend</label>
+            </li>
+            <li>
+              <input type="checkbox" name="type[]" id="back" value="back" onclick="gestionFiltre('BACKEND')">
+              <label for="back">Backend</label>
+            </li>
+            <li>
+              <input type="checkbox" name="type[]" id="heb" value="heb" onclick="gestionFiltre('HEBERGEMENT')">
+              <label for="heb">Hébergement</label>
+            </li>
+          </ul>
+        </fieldset>
+        <fieldset>
+          <legend>Type</legend>
+          <ul id="liste_filtres">
+            <li>
+              <input type="checkbox" name="type[]" id="reco" value="reco" onclick="">
+              <label for="reco">Recommandation</label>
+            </li>
+            <li>
+              <input type="checkbox" name="type[]" id="cons" value="cons" onclick="">
+              <label for="cons">Conseil</label>
+            </li>
+        </fieldset>
+        <fieldset>
+          <legend>Impact personnes</legend>
+          <ul id="liste_filtres">
+            <li>
+              <input type="checkbox" name="type[]" id="peopleA" value="peopleA" onclick="">
+              <label for="peopleA">A</label>
+            </li>
+            <li>
+              <input type="checkbox" name="type[]" id="peopleB" value="peopleB" onclick="">
+              <label for="peopleB">B</label>
+            </li>
+            <li>
+              <input type="checkbox" name="type[]" id="peopleC" value="peopleC" onclick="">
+              <label for="peopleC">C</label>
+            </li>
+          </ul>
+        </fieldset>
+        <fieldset>
+          <legend>Impact planète</legend>
+          <ul id="liste_filtres">
+            <li>
+              <input type="checkbox" name="type[]" id="planetA" value="planetA" onclick="">
+              <label for="planetA">A</label>
+            </li>
+            <li>
+              <input type="checkbox" name="type[]" id="planetB" value="planetB" onclick="">
+              <label for="planetB">B</label>
+            </li>
+            <li>
+              <input type="checkbox" name="type[]" id="planetC" value="planetC" onclick="">
+              <label for="planetC">C</label>
+            </li>
+          </ul>
+        </fieldset>
+        <fieldset>
+          <legend>Impact prospérité</legend>
+          <ul id="liste_filtres">
+            <li>
+              <input type="checkbox" name="type[]" id="prosperityA" value="prosperityA" onclick="">
+              <label for="prosperityA">A</label>
+            </li>
+            <li>
+              <input type="checkbox" name="type[]" id="prosperityB" value="prosperityB" onclick="">
+              <label for="prosperityB">B</label>
+            </li>
+            <li>
+              <input type="checkbox" name="type[]" id="prosperityC" value="prosperityC" onclick="">
+              <label for="prosperityC">C</label>
+            </li>
+          </ul>
+        </fieldset>
+        <fieldset>
+          <legend>Difficulté de mise en oeuvre</legend>
+          <ul id="liste_filtres">
+            <li>
+              <input type="checkbox" name="type[]" id="difficulty-N/A" value="difficulty-N/A" onclick="">
+              <label for="difficulty-N/A">N/A</label>
+            </li>
+            <li>
+              <input type="checkbox" name="type[]" id="difficulty-facile" value="difficulty-facile" onclick="">
+              <label for="difficulty-facile">Facile</label>
+            </li>
+            <li>
+              <input type="checkbox" name="type[]" id="difficulty-mid" value="difficulty-mid" onclick="">
+              <label for="difficulty-mid">Modérée</label>
+            </li>
+            <li>
+              <input type="checkbox" name="type[]" id="difficulty-hard" value="difficulty-hard" onclick="">
+              <label for="difficulty-hard">Difficile</label>
+            </li>
+          </ul>
+        </fieldset>
+
+
+
+
+
+
         <div>
           <p>charte graphique</p>
           <div class="graphique1">.</div>
@@ -67,12 +161,14 @@
       </form>
 
       <article>
+      <h2 class="titre">"compteur" critères</h2>
+      <p class="titre">Sélectionnez les bonnes pratiques que vous souhaitez ajouter à votre projet parmis les 484 critères du référentiel INR.</p>
+
         <ul class="main" id="ulp">
           <!--ECRIRE ICI bonnes pratiques-->
-          <h2>Sélectionnez vos bonnes pratiques</h2>
+          
         </ul>
-        <p>text ...</p>
-
+        
       </article>
 
       <script src="main.js"></script>

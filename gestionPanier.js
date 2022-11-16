e0116789bffdbbe95c1d14a3440a4d38693491ca
Jupiiii <hillereau.clement@orange.fr>
2022-11-16 14:19:29 +0100

    save

--- gestionPanier.js
+++ gestionPanier.js
@@ -1,1 +1,140 @@
-var requestURL="pratiquesFROMexcel.json",request=new XMLHttpRequest;request.open("GET",requestURL),request.responseType="json",request.send();var pratiques=request.response;request.onload=function(){affichePanier()};var panier=[];function afficheArticle(e){let t;(t="R\xe9alisation"===e.cycleVie?document.getElementById("rea"):"Utilisation"===e.cycleVie?document.getElementById("util"):"Conception"===e.cycleVie?document.getElementById("concep"):"Acquisition"===e.cycleVie?document.getElementById("acqui"):"D\xe9ploiement"===e.cycleVie?document.getElementById("deploi"):"Administration"===e.cycleVie?document.getElementById("admi"):"Revalorisation"===e.cycleVie?document.getElementById("revalo"):"N/A"===e.cycleVie?document.getElementById("N/A"):"Maintenance"===e.cycleVie?document.getElementById("maintenance"):document.getElementById("end")).classList.add("ul-decalage");let n=document.createElement("li"),a=document.createElement("article"),i=document.createElement("div"),l=document.createElement("span"),r=document.createElement("span"),s=document.createElement("span"),c=document.createElement("span"),d=document.createElement("span"),p=document.createElement("span"),o=document.createElement("span"),u=document.createElement("h3");document.createElement("hr"),n.setAttribute("id",e.ID),n.classList.add("LiListeItem"),s.classList.add("attributs","incontournable"),c.classList.add("attributs"),d.classList.add("attributs"),p.classList.add("attributs"),o.classList.add("attributs"),l.textContent=e.type+" "+e.famille,c.textContent=e.people,d.textContent=e.planet,p.textContent=e.prosperity,o.textContent=e.miseEnOeuvre,u.textContent=e.criteres,t.appendChild(n),n.appendChild(a),a.appendChild(i),i.appendChild(l),l.appendChild(r),null!=e.incontournable&&(s.textContent=e.incontournable,r.appendChild(s)),r.appendChild(c),r.appendChild(d),r.appendChild(p),r.appendChild(o),i.appendChild(u)}function affichePanier(){for(var e=[],t=request.response.listePratiques,n=0;n<t.length;n++)null!=t[n].incontournable&&e.push(n);for(var n=0;n<e.length;n++)afficheArticle(t[e[n]]);if(null!=localStorage.getItem("panier"))for(var a=Array.from(JSON.parse(localStorage.getItem("panier"))),n=0;n<a.length;n++)afficheArticle(t[a[n]])}function vider(){localStorage.clear(),location.reload()}null!=localStorage.getItem("panier")&&(panier=Array.from(JSON.parse(localStorage.getItem("panier"))));
+var requestURL = "pratiquesFROMexcel.json";
+var request = new XMLHttpRequest();
+
+request.open("GET", requestURL);
+request.responseType = "json";
+request.send();
+var pratiques = request.response;
+
+request.onload = function () {
+  affichePanier();
+};
+
+//Le panier en local storage, on ne le reset pas si non vide
+var panier = [];
+//Le panier ne contient que les ID des articles
+if (localStorage.getItem("panier") != null) {
+  panier = Array.from(JSON.parse(localStorage.getItem("panier")));
+}
+
+function afficheArticle(jsonObj) {
+  let ul;
+  /*
+  If|Else → ordre décroissant du nombre de critère par type CYCLE DE VIE : 
+  N/A			        23
+  Acquisition	    53
+  Conception	    82
+  Réalisation	    93
+  Déploiement	    48
+  Administration	45
+  Utilisation	    88
+  Maintenance		  16
+  Fin de Vie		  17
+  Revalorisation	26
+
+ICI "fin de vie" marche pas → on le met en ELSE
+*/
+  if (jsonObj["cycleVie"] === "Réalisation") {
+    ul = document.getElementById("rea");
+  } else if (jsonObj["cycleVie"] === "Utilisation") {
+    ul = document.getElementById("util");
+  } else if (jsonObj["cycleVie"] === "Conception") {
+    ul = document.getElementById("concep");
+  } else if (jsonObj["cycleVie"] === "Acquisition") {
+    ul = document.getElementById("acqui");
+  } else if (jsonObj["cycleVie"] === "Déploiement") {
+    ul = document.getElementById("deploi");
+  } else if (jsonObj["cycleVie"] === "Administration") {
+    ul = document.getElementById("admi");
+  } else if (jsonObj["cycleVie"] === "Revalorisation") {
+    ul = document.getElementById("revalo");
+  } else if (jsonObj["cycleVie"] === "N/A") {
+    ul = document.getElementById("N/A");
+  } else if (jsonObj["cycleVie"] === "Maintenance") {
+    ul = document.getElementById("maintenance");
+  } else {
+    //FIN DE VIE
+    ul = document.getElementById("end");
+  }
+
+  ul.classList.add("ul-decalage");
+  let li = document.createElement("li");
+  let article = document.createElement("article");
+  let divArticle = document.createElement("div");
+  /*form.setAttribute("data-screen-only",""); INDICATEURS*/
+  let spanLigne = document.createElement("span");
+  let spanAttributs = document.createElement("span");
+  let spanIncontournable = document.createElement("span");
+  let spanPeople = document.createElement("span");
+  let spanPlanet = document.createElement("span");
+  let spanProsperity = document.createElement("span");
+  let spanDifficulty = document.createElement("span");
+
+  let critere = document.createElement("h3");
+  let hrLIGNE = document.createElement("hr");
+  //donne l'id au bloc
+  li.setAttribute("id", jsonObj["ID"]);
+  li.classList.add("LiListeItem");
+
+  spanIncontournable.classList.add("attributs","incontournable");
+  spanPeople.classList.add("attributs");
+  spanPlanet.classList.add("attributs");
+  spanProsperity.classList.add("attributs");
+  spanDifficulty.classList.add("attributs");
+  //Creation des textes des éléments
+  spanLigne.textContent = jsonObj["type"] + " " + jsonObj["famille"];
+  spanPeople.textContent = jsonObj["people"];
+  spanPlanet.textContent = jsonObj["planet"];
+  spanProsperity.textContent = jsonObj["prosperity"];
+  spanDifficulty.textContent = jsonObj["miseEnOeuvre"];
+  critere.textContent = jsonObj["criteres"];
+
+  
+  //Structuration
+  ul.appendChild(li);
+  li.appendChild(article);
+  article.appendChild(divArticle);
+  divArticle.appendChild(spanLigne);
+  spanLigne.appendChild(spanAttributs);
+  //SI critere INCONTOURNABLE :
+  if(jsonObj['incontournable']!=null){
+    spanIncontournable.textContent = jsonObj["incontournable"];
+    spanAttributs.appendChild(spanIncontournable);
+  }
+  spanAttributs.appendChild(spanPeople);
+  spanAttributs.appendChild(spanPlanet);
+  spanAttributs.appendChild(spanProsperity);
+  spanAttributs.appendChild(spanDifficulty);
+  divArticle.appendChild(critere);
+}
+
+//pour afficher
+function affichePanier() {
+  var indexIncontournable = [];
+  //AJOUT CRITERES INCONTOURNABLES :
+  var pratiques = request.response;
+  var prat = pratiques["listePratiques"];
+  for (var i = 0; i < prat.length; i++) {
+    if (prat[i]["incontournable"] != null) {
+      indexIncontournable.push(i); //ajoute l'index des criteres incontournables
+    }
+  }
+  //affichage CRITERES INCONTOURNABLES :
+  for (var i = 0; i < indexIncontournable.length; i++) {
+    afficheArticle(prat[indexIncontournable[i]]);
+  }
+
+  //ajout critères manuellement LOCALSTORAGE
+  if (localStorage.getItem("panier")!= null){
+    var panierSauv = Array.from(JSON.parse(localStorage.getItem("panier")));
+    for (var i = 0; i < panierSauv.length; i++) {
+      afficheArticle(prat[panierSauv[i]]);
+    }
+  }
+  
+}
+
+function vider() {
+  localStorage.clear();
+  location.reload();
+}
